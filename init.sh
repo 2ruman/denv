fd467810a1c150c57f218c67047c845dc036a2b1
2ruman <truman.t.kim@gmail.com>
2026-03-17 04:13:40 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function exit_err() {
+    if [ $? -ne 0 ]; then
+        echo "Failed to set up your world"
+        exit 1
+    fi
+}
+
 U="truman"
 
 if [ $USER != "$U" ]; then
@@ -20,9 +27,14 @@ sudo -E env "U=$U" "HOME=$HOME" sh -c '(grep -q "Truman-added" /etc/fstab && ech
 (printf "\n# Truman-added\n$HOME/world\t/$U\tnone\tdefaults,bind\t0\t0\n" >> /etc/fstab && echo "Done")'
 
 sudo mount -a && sudo systemctl daemon-reload
-if [ $? -ne 0 ]; then
-    echo "Failed to set up your world"
-    exit 1
-fi
+exit_err
+
+# Read Only 2ruman
+RO2=/$U/.2ruman
+
+mkdir $RO2
+sudo chattr -a $RO2/
+mv ~/dev-env/ $RO2/
+cd $RO2/ && git clone https://github.com/2ruman/linux-programming.git
 
 echo "Done"
