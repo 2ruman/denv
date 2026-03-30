1734cd3360a1c48a2a0145eca3a7179e77571704
2ruman <truman.t.kim@gmail.com>
2026-03-30 09:41:08 +0900

    inst: Update inst-ans.sh

--- inst/inst-ans.sh
+++ inst/inst-ans.sh
@@ -1,42 +1,116 @@
 #!/bin/bash
 
+source $(realpath $(dirname "${BASH_SOURCE[0]}"))/inst.func
+
 HTTPS_DEV_ANS_ADDR="https://developer.android.com/studio"
 ANS_ARC_LINUX_URL_PTN="^[[:space:]]*href=\"https://.+android-studio-.+linux.tar.gz"
 
-function get_dev_ans_html() {
+_get_dev_ans_addr() {
+    echo "$HTTPS_DEV_ANS_ADDR"
+}
+
+_get_dev_ans_html() {
     wget -qO - $HTTPS_DEV_ANS_ADDR 2>/dev/null
 }
 
-function get_ans_arc_addr() {
+_get_ans_arc_addr() {
     local -
     set -euo pipefail
 
-    (get_dev_ans_html | grep -oE $ANS_ARC_LINUX_URL_PTN | sed -E 's|^[[:space:]]*href="||') 2>/dev/null
+    (_get_dev_ans_html | grep -oE $ANS_ARC_LINUX_URL_PTN | sed -E 's|^[[:space:]]*href="||') 2>/dev/null
 }
 
-arc_dest=${1:-~/Downloads}
-if [ ! -d "$arc_dest" ]; then
-    echo "Invalid dest directory: $arc_dest"
+_logo() {
+    echo -e "${NOC}\
+-------------------------------------------------------------------------
+    ${GRN}"
+cat << 'EOF'
+     _              _           _     _   ____  _             _ _
+    / \   _ __   __| |_ __ ___ (_) __| | / ___|| |_ _   _  __| (_) ___
+   / _ \ | '_ \ / _` | '__/ _ \| |/ _` | \___ \| __| | | |/ _` | |/ _ \
+  / ___ \| | | | (_| | | | (_) | | (_| |  ___) | |_| |_| | (_| | | (_) |
+ /_/   \_\_| |_|\__,_|_|  \___/|_|\__,_| |____/ \__|\__,_|\__,_|_|\___/
+
+ ___           _        _ _
+ |_ _|_ __  ___| |_ __ _| | | ___ _ __
+  | || '_ \/ __| __/ _` | | |/ _ \ '__|
+  | || | | \__ \ || (_| | | |  __/ |
+ |___|_| |_|___/\__\__,_|_|_|\___|_|
+
+EOF
+    echo -e "${NOC}\
+-------------------------------------------------------------------------
+    ${NOC}"
+}
+
+_logo
+
+while getopts ":fi:d:" opt; do
+    case $opt in
+        f) i_mode=false ;;
+        i) inst_dest=$OPTARG ;;
+        d) dl_dest=$OPTARG ;;
+        *) exit_err "Invalid option: -$OPTARG" ;;
+        :) exit_err "Option -$OPTARG requires an argument" ;;
+    esac
+done
+shift $(($OPTIND - 1))
+
+i_mode=${i_mode:=true}
+inst_dest=${inst_dest:=${MY_TOOLS:-}}
+dl_dest="${dl_dest:=~/Downloads}/inst-ans"
+
+[ -z "$inst_dest" ] && exit_err "Please provide directory to install or set \$MY_TOOLS variable beforehand"
+
+log_mark "Download directory: ${YLW}${dl_dest}${NOC}"
+log_mark "Installation directory: ${YLW}$inst_dest${NOC}"
+[ $i_mode ] && ask_cnfm
+
+log_step "Preparing download directory..."
+[ ! -d "$dl_dest" ] && exit_of mkdir -p "$dl_dest"
+
+log_step "Finding archive file on ${YLW}$(_get_dev_ans_addr)${NOC}"
+arc_addr=$(_get_ans_arc_addr)
+[ -z "$arc_addr" ] && exit_err "Archive file not found"
+
+# log_step "Downloading archive file..."
+arc_file=$(echo $arc_addr | sed -E 's/^.+\///')
+[[ $? != 0 || -z $arc_file ]] && exit_err "Failed to get archive file name..."
+
+log_mark "Archive file name: ${YLW}$arc_file${NOC}"
+log_mark "Archive file destination: ${YLW}$dl_dest${NOC}"
+log_over "Download will begin in 3 seconds..."
+sleep 3s
+
+log_step "Downloading archive file..."
+wget -c -O "$dl_dest/$arc_file" "$arc_addr" || exit_err "Failed..."
+
+log_step "Checking archive file..."
+ans_dir_checked=$(tar -ztf "$dl_dest/$arc_file" | head -n1 | sed 's/\/$//')
+[ -z "$ans_dir_checked" ] && exit_err "Cannot find directory in archive file"
+
+log_mark "Directory in archive file: ${YLW}$ans_dir_checked${NOC}"
+
+
+log_step "Checking destination directory..."
+ans_tobe="$inst_dest/$ans_dir_checked"
+ans_old="$inst_dest/${ans_dir_checked}.old"
+if [ -e "$inst_dest/$ans_dir_checked" ]; then
+    log_mark "Already exists: ${YLW}$ans_tobe${NOC}"
+    log_step "Moving this to: ${YLW}$ans_old${NOC}"
+    [ $i_mode ] && ask_cnfm
+
+    [ -e "$ans_old" ] && exit_err "Already exists: ${YLW}$ans_old${NOC}"
+    mv "$ans_tobe" "$ans_old"
 fi
 
-arc_addr=$(get_ans_arc_addr)
+log_step "Decompressing archive file..."
+ans_tobe="$inst_dest/$ans_dir_checked"
+exit_of "tar -zxf $dl_dest/$arc_file -C $inst_dest/"
 
-if [ -n "$arc_addr" ]; then
-    echo "Archive found at: " "$arc_addr"
+log_step "Removing archive file..."
+rm_cmd="rm -rf $dl_dest"
+log_mark "Prompting: ${YLW}$rm_cmd${NOC}"
+[ $i_mode ] && ask_cnfm && exit_of "$rm_cmd"
 
-    arc_file=$(echo $arc_addr | sed -E 's/^.+\///')
-    if [[ $? != 0 || -z $arc_file ]]; then
-        echo "Failed to get archive file name..."
-    else
-        echo "Archive file name: $arc_file"
-        echo "Archive destination: $arc_dest"
-        echo "Download will begin in 3 seconds..."
-        sleep 3s
-    fi
-    wget -O "$arc_dest/$arc_file" "$arc_addr"
-else
-    echo "Archive not found..."
-    exit 1
-fi
-
-echo "Done"
+log_mark "Done."
